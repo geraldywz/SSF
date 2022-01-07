8f940866a1ffe9fb92de5d1505b8b788c810b05c
Gerald Yeo <ywz.gerald@gmail.com>
2022-01-07 09:30:05 +0800

    Initial Commit of D13.

---
+++ D13/src/test/java/ssf/d13/D13.java
@@ -0,0 +1,13 @@
+package ssf.d13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D13 {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
