22b9dd03922486eead864686d972d022e85bc5e7
Gerald Yeo <ywz.gerald@gmail.com>
2022-01-08 21:28:56 +0800

    Minor Updates.

--- D13/src/main/java/ssf/d13/Main.java
+++ D13/src/main/java/ssf/d13/Main.java
@@ -1,62 +1,19 @@
 package ssf.d13;
 
-import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static ssf.d13.util.Constants.*;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import ssf.d13.util.Butler;
 
 @SpringBootApplication
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
-        boolean dataDirExist = false;
-        for (String arg : args) {
-            logger.info("Arg" + LOG_DELINEATOR + arg);
-        }
-        SpringApplication springApp = new SpringApplication(Main.class);
-        DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
+        SpringApplication springApp = new SpringApplication(Main.class);        
 
-        if (appArgs.containsOption(OPTION_DATA_DIR)) {
-            List<String> dataDir = appArgs.getOptionValues(OPTION_DATA_DIR);
-            logger.info("Path" + LOG_DELINEATOR + dataDir);
-            for (String p : dataDir) {
-                Path path = Paths.get(p);
-                File file = path.toFile();
-                dataDirExist = file.isDirectory();
-                if (dataDirExist) {
-                    logger.info(file.toString() + LOG_DELINEATOR + "exists.");
-                } else {
-                    logger.info(file.toString() + LOG_DELINEATOR + "not found.");
-                    logger.info("Attempting to create missing folders...");
-                    dataDirExist = file.mkdirs();
-                    if (dataDirExist) {
-                        logger.info(file.toString() + LOG_DELINEATOR + "created.");
-                    } else {
-                        logger.info(file.toString() + LOG_DELINEATOR + "failed to create.");
-                        break;
-                    }
-                }
-                if (dataDirExist) {
-                    DOC_ROOT.add(path);
-                    // springApp.setDefaultProperties(Collections.singletonMap("server.port",portNumber));
-                }
-            }
-        }
-
-        if (dataDirExist) {
+        if (Butler.checkDataDir(args)) {
             springApp.run(args);
         } else {
-            logger.info("Terminating Process.");
+            Butler.log("Terminating Process.");
             System.exit(1);
         }
     }
---
+++ D13/src/main/java/ssf/d13/util/Butler.java
@@ -0,0 +1,65 @@
+package ssf.d13.util;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.DefaultApplicationArguments;
+
+public class Butler {
+
+    private static final String OPTION_DATA_DIR = "dataDir";
+    private static final String LOG_DELINEATOR = " >>>>> ";
+    private static final List<Path> DOC_ROOT = new ArrayList<>();
+
+    private static final Logger logger = LoggerFactory.getLogger(Butler.class);
+
+    public static void log(String subject, String action) {
+        logger.info(subject + LOG_DELINEATOR + action);
+    }
+
+    public static void log(String msg) {
+        logger.info(msg);
+    }
+
+    public static boolean checkDataDir(String[] args) {
+        boolean dataDirExists = false;
+
+        DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
+        for (String arg : args) {
+            log("Arg", arg);
+        }
+        if (appArgs.containsOption(OPTION_DATA_DIR)) {
+            List<String> dataDir = appArgs.getOptionValues(OPTION_DATA_DIR);
+            dataDir.forEach(string -> {
+                log("Path", string);
+            });
+            for (String p : dataDir) {
+                Path path = Paths.get(p);
+                File file = path.toFile();
+                dataDirExists = file.isDirectory();
+                if (dataDirExists) {
+                    log(file.toString(), "exists.");
+                } else {
+                    log(file.toString(), "not found.");
+                    log("Attempting to create missing folders...");
+                    dataDirExists = file.mkdirs();
+                    if (dataDirExists) {
+                        log(file.toString(), "created.");
+                    } else {
+                        log(file.toString(), "failed to create.");
+                        break;
+                    }
+                }
+                if (dataDirExists) {
+                    DOC_ROOT.add(path);
+                }
+            }
+        }
+        return dataDirExists;
+    }
+}
